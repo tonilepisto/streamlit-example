55b2d1cf2de2f29168646162bd1e64adf7c1aeb9
tonilepisto <60813165+tonilepisto@users.noreply.github.com>
2022-01-05 14:06:51 +0200

    Update streamlit_app.py
    App cleanup

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,19 +14,12 @@ from dateutil import tz
 from bokeh.plotting import figure
 
 """
-# Welcome to Streamlit!
+# Welcome to Data app
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have some other more questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-
-And check out:
-#https://github.com/MichaelAllen1966/2109_streamlit_examples
-#https://medium.com/@avra42/streamlit-python-cool-tricks-to-make-your-web-application-look-better-8abfc3763a5b
-#https://www.youtube.com/watch?v=G9U4Uixssf0&t=3s
+Check out:
+https://github.com/MichaelAllen1966/2109_streamlit_examples
+https://medium.com/@avra42/streamlit-python-cool-tricks-to-make-your-web-application-look-better-8abfc3763a5b
+https://www.youtube.com/watch?v=G9U4Uixssf0&t=3s
 
 """
 
@@ -38,29 +31,6 @@ And check out:
 #        </style>
 #        """
 #st.markdown(hide_menu_style, unsafe_allow_html=True)
-
-"""
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
-"""
 
 # Add a title
 st.title('Data to dataframe app')
