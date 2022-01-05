34acdf9dfb4327a4e85c599d9b578d6fdf38bc92
tonilepisto <60813165+tonilepisto@users.noreply.github.com>
2022-01-05 14:07:37 +0200

    Update streamlit_app.py
    cleanup 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -179,14 +179,14 @@ df2 = pd.DataFrame(
 #st.line_chart(chart_data)
 
 #Bokeh example
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
+#x = [1, 2, 3, 4, 5]
+#y = [6, 7, 2, 4, 5]
 
-p = figure(
-     title='simple line example',
-     x_axis_label='x',
-     y_axis_label='y')
+#p = figure(
+#     title='simple line example',
+#     x_axis_label='x',
+#     y_axis_label='y')
 
-p.line(x, y, legend_label='Trend', line_width=2)
+#p.line(x, y, legend_label='Trend', line_width=2)
 
-st.bokeh_chart(p, use_container_width=True)
+#st.bokeh_chart(p, use_container_width=True)
