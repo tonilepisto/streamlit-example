f0213ffc84beb6e66c49abc977ecf8c2c5e992cb
tonilepisto <60813165+tonilepisto@users.noreply.github.com>
2022-01-06 22:10:33 +0200

    Update streamlit_app.py
    column example added

--- streamlit_app.py
+++ streamlit_app.py
@@ -196,6 +196,18 @@ option = st.selectbox(
 
 'You selected: ', option
 
+
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
+    
 #my_table.add_rows(df2)
 
 #chart_data = pd.DataFrame(
